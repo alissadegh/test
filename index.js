e7faa8287a93c6272e6d968b2996781cccfe5441
alissadegh <ali.sadeghi.0035tizhosh89@gmail.com>
2026-06-29 23:47:24 +0330

    adding function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 const student = {
-    hight: "farsi",g
+    hight: "farsi", g
     weight: 24,
     size: 24,
+    lgkksldsfg: fkjasdfjkasd
     listar: [1, 2, 3.3]
 }
 const list = [student, 2, 3, 4, 5, 6, 7]
