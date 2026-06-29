398dfb437165a48f4cd1c3e62e915d51a8a067e9
alissadegh <ali.sadeghi.0035tizhosh89@gmail.com>
2026-06-29 23:43:45 +0330

    fsd

---
+++ app.py
@@ -0,0 +1,10 @@
+age = 16
+print(age)
+are = True
+print(are)
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,11 @@
+
+const student = {
+    hight: "farsi",g
+    weight: 24,
+    size: 24,
+    listar: [1, 2, 3.3]
+}
+const list = [student, 2, 3, 4, 5, 6, 7]
+function myfun(student) {
+    console.log(student.size)
+}
